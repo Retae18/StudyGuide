e3003318008691e4c4ec3e682bdc8477c4559a0a
Retae18 <35357413+Retae18@users.noreply.github.com>
2018-01-24 22:43:21 -0500

    Add files via upload
    Study guide on conversions of data types

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StudyGuide
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var number = 12;                //int
+            var price = 12.56f;             //float
+            var vowel = true;               //bool
+            var character = 'S';            //char
+            var lastName = "Adkins";        //string
+
+
+            Console.WriteLine(number);
+            Console.WriteLine(price);
+            Console.WriteLine(vowel);
+            Console.WriteLine(character);
+            Console.WriteLine(lastName);
+
+
+        }
+   
+    
+    }
+    // Conversion Notes
+
+    /* Implicit Conversion Examples:
+     
+     byte b= 1; --> int i = b;  (no Data Loss)
+     int i = 1; --> float f = i;  (no Data Loss)
+
+    Explicit Conversion:
+    
+    int i = 1; byte b= i;
+    (possible data loss, if more data than what there's room for)
+
+    Non Compatible types:
+
+    string s = "1";
+    int i - Convert.ToInt32(s);
+    int j = int.Parse(s); */
+
+    
+
+
+
+
+
+}
